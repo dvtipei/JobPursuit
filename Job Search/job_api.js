fb970063a18bb7c4aadcf2b9ea0e0c9d51102d2d
jaguilar0047@gmail.com <Jaguilar0047@gmail.com>
2022-07-28 14:39:47 -0700

    node

--- Job Search/job_api.js
+++ Job Search/job_api.js
@@ -1,21 +1,31 @@
+const router = require("express").Router();
 const axios = require("axios");
 
-const options = {
-  method: "POST",
-  url: "https://linkedin-jobs-search.p.rapidapi.com/",
-  headers: {
-    "content-type": "application/json",
-    "X-RapidAPI-Key": "ee4ba4d88emsh5f022dd384ca11cp1c3043jsn048617c942bf",
-    "X-RapidAPI-Host": "linkedin-jobs-search.p.rapidapi.com",
-  },
-  data: '{"search_terms":"python programmer","location":"30301","page":"1"}',
-};
+router.post("/", async (req, res) => {
+  try {
+    const options = {
+      method: "POST",
+      url: "https://linkedin-jobs-search.p.rapidapi.com/",
+      headers: {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "ee4ba4d88emsh5f022dd384ca11cp1c3043jsn048617c942bf",
+        "X-RapidAPI-Host": "linkedin-jobs-search.p.rapidapi.com",
+      },
+      data: `{"search_terms":"${}","location":"${}","page":"1"}`,
+    };
 
-axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
