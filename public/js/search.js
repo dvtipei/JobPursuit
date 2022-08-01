3f0f401e667f864bdc49192bbbc8487bb0f79e0b
jaguilar0047@gmail.com <Jaguilar0047@gmail.com>
2022-08-01 13:01:00 -0700

    save

--- public/js/search.js
+++ public/js/search.js
@@ -19,24 +19,7 @@ function jobs() {
     .then((response) => {
       console.log(response);
       for (let i = 0; i < response.length; i++) {
-        // var jobTitle = document.createElement("h2");
-        // var jobLink = document.createElement("a");
-        // var jobImage = document.createElement("img");
-        // jobImage.setAttribute(
-        //   "src",
-        //   "https://play-lh.googleusercontent.com/kMofEFLjobZy_bCuaiDogzBcUT-dz3BBbOrIEjJ-hqOabjK8ieuevGe6wlTD15QzOqw"
-        // );
-        // jobImage.style.width = "50px";
-        // jobLink.setAttribute("href", response[i].job_url);
-        // jobLink.appendChild(jobImage);
-        // //jobTitle.setAttribute("class", "red");
-        // //=jobDescription.setAttribute("class", "");
-        // jobTitle.textContent = response[i].job_title;
-        // jobLink.textContent = response[i].job_url;
-        // div.appendChild(jobTitle);
-        // div.appendChild(jobLink);
         var card_html = template;
-        card_html = card_html.replace("{href2}", response[i].job_url);
         card_html = card_html.replace("{href}", response[i].job_url);
         card_html = card_html.replace("{title}", response[i].job_title);
         card_html = card_html.replace("data-src", "src");
