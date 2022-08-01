fd5b5899d378062e3bfe1a2fa9e5e42c03207b99
smulrow <smulrow@gmail.com>
2022-08-01 10:34:14 -0700

    adka

---
+++ KanBanBoard/js/view/Kanban.js
@@ -0,0 +1,34 @@
+import Column from "./Column.js";
+
+export default class Kanban {
+	constructor(root) {
+		this.root = root;
+
+		Kanban.columns().forEach(column => {
+			const columnView = new Column(column.id, column.title);
+
+			this.root.appendChild(columnView.elements.root);
+		});
+	}
+
+	static columns() {
+		return  [
+			{
+				id: 1,
+				title: "Wishlist"
+			},
+			{
+				id: 2,
+				title: "Applied"
+			},
+			{
+				id: 3,
+				title: "Interview"
+			},
+			{
+				id: 4,
+				title: "Offer"
+			},
+		];
+	}
+}
--- public/js/login.js
+++ public/js/login.js
@@ -30,10 +30,12 @@ const signupFormHandler = async (event) => {
       method: 'POST',
       body: JSON.stringify({ username, email, password }),
       headers: { 'Content-Type': 'application/json' },
+      
     });
 
     if (response.ok) {
       document.location.replace('/');
+      alert('You have successfully signed up! Please sign in!');
     } else {
       alert('Failed to sign up.');
     }
