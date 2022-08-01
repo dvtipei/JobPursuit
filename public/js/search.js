e979f827cb6864b40b7a3e492732488f3c8ad4aa
jaguilar0047@gmail.com <Jaguilar0047@gmail.com>
2022-08-01 12:57:16 -0700

    save

--- public/js/search.js
+++ public/js/search.js
@@ -1,5 +1,6 @@
 var div = document.getElementById("jobs");
 var btn = document.getElementById("form1");
+var template = document.querySelector("#my-template").innerHTML;
 
 function jobs() {
   var jobSearch = document.getElementById("jobSearch");
@@ -18,14 +19,31 @@ function jobs() {
     .then((response) => {
       console.log(response);
       for (let i = 0; i < response.length; i++) {
-        var jobTitle = document.createElement("h2");
-        var jobDescription = document.createElement("p");
-        //jobTitle.setAttribute("class", "red");
-        //jobDescription.setAttribute("class", "");
-        jobTitle.textContent = response[i].job_title;
-        jobDescription.textContent = response[i].job_url;
-        div.appendChild(jobTitle);
-        div.appendChild(jobDescription);
+        // var jobTitle = document.createElement("h2");
+        // var jobLink = document.createElement("a");
+        // var jobImage = document.createElement("img");
+        // jobImage.setAttribute(
+        //   "src",
+        //   "https://play-lh.googleusercontent.com/kMofEFLjobZy_bCuaiDogzBcUT-dz3BBbOrIEjJ-hqOabjK8ieuevGe6wlTD15QzOqw"
+        // );
+        // jobImage.style.width = "50px";
+        // jobLink.setAttribute("href", response[i].job_url);
+        // jobLink.appendChild(jobImage);
+        // //jobTitle.setAttribute("class", "red");
+        // //=jobDescription.setAttribute("class", "");
+        // jobTitle.textContent = response[i].job_title;
+        // jobLink.textContent = response[i].job_url;
+        // div.appendChild(jobTitle);
+        // div.appendChild(jobLink);
+        var card_html = template;
+        card_html = card_html.replace("{href2}", response[i].job_url);
+        card_html = card_html.replace("{href}", response[i].job_url);
+        card_html = card_html.replace("{title}", response[i].job_title);
+        card_html = card_html.replace("data-src", "src");
+
+        var card = document.createElement("div");
+        card.innerHTML = card_html;
+        div.appendChild(card);
       }
     })
     .catch((err) => console.error(err));
