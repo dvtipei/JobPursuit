2cdbae4a069fac2fa476559e6dedbc94e1c2d1e4
smulrow <smulrow@gmail.com>
2022-08-01 11:54:00 -0700

    pulling new main

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -30,25 +30,6 @@ router.get('/', withAuth, async (req, res) => {
 // }
 // });
 
-// try {
-//     const userData = await User.findAll({
-//     attributes: { exclude: ['password'] },
-//     order: [['ASC']],
-//     });
-
-//     const users = userData.map((project) => project.get({ plain: true }));
-
-//     res.render('homepage', {
-//     users,
-//       // Pass the logged in flag to the template
-//     logged_in: req.session.logged_in,
-//     });
-// } catch (err) {
-//     res.status(500).json(err);
-// }
-// });
-
-
 router.get('/login', (req, res) => {
   // If a session exists, redirect the request to the homepage
 if (req.session.logged_in) {
--- public/js/login.js
+++ public/js/login.js
@@ -1,48 +1,51 @@
 const loginFormHandler = async (event) => {
+  // Stop the browser from submitting the form so we can do so with JavaScript
   event.preventDefault();
 
+  // Gather the data from the form elements on the page
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
+    // Send the e-mail and password to the server
     const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
-      
     });
 
     if (response.ok) {
       document.location.replace('/');
-      alert('You have successfully signed up! Please sign in!');
     } else {
-      alert('Failed to log in.');
+      alert('Failed to log in');
     }
   }
 };
 
 const signupFormHandler = async (event) => {
-  event.preventDefault();
+event.preventDefault();
 
-  const email = document.querySelector('#email-create').value.trim();
-  const password = document.querySelector('#password-create').value.trim();
+const email = document.querySelector('#email-create').value.trim();
+const password = document.querySelector('#password-create').value.trim();
 
-  if ( email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ username, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-      
-    });
+if (email && password) {
+  const response = await fetch('/api/users', {
+    method: 'POST',
+    body: JSON.stringify({ email, password }),
+    headers: { 'Content-Type': 'application/json' },
+    
+  });
 
-    if (response.ok) {
-      document.location.replace('/');
-      alert('You have successfully signed up! Please sign in!');
-    } else {
-      alert('Failed to sign up.');
-    }
+  if (response.ok) {
+    document.location.replace('/');
+    alert('You have successfully signed up! Please sign in!');
+  } else {
+    alert('Failed to sign up.');
   }
+}
 };
+
+
 
 document
   .querySelector('.login-form')
@@ -51,3 +54,5 @@ document
 document
   .querySelector('.create-form')
   .addEventListener('submit', signupFormHandler);
+
+
